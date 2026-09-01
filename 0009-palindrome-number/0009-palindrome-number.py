154235eb2d8c5856d3c8a931bf9b0f79687b5108
Prashant Singh <prashantsinghsingh2009@gmail.com>
2026-09-01 19:15:46 +0530

    Time: 4 ms (90.8%), Space: 12.4 MB (18.62%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,14 +4,5 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        if x < 0 or (x % 10 == 0 and x != 0):
-            return False
-
-        reversed_half = 0
-        while x > reversed_half:
-            reversed_half = reversed_half * 10 + x % 10
-            x //= 10
-
-        # For even length: x == reversed_half
-        # For odd length: x == reversed_half // 10 (discards middle digit)
-        return x == reversed_half or x == reversed_half // 10
+        s = str(x)
+        return s == s[::-1]
