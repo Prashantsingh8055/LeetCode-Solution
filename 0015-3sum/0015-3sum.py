b7a26c0ef13320a3f90a9339d44c0e85184d4109
Prashant Singh <prashantsinghsingh2009@gmail.com>
2026-09-07 21:32:33 +0530

    Time: 552 ms (96.16%), Space: 18.2 MB (81.59%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        res = []
+        n = len(nums)
+
+        for i in range(n - 2):
+            # If the smallest number is greater than 0, no triplet can sum to 0
+            if nums[i] > 0:
+                break
+
+            # Skip duplicate values for the first element
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            left = i + 1
+            right = n - 1
+
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    res.append([nums[i], nums[left], nums[right]])
+
+                    # Skip duplicates for the second element
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    # Skip duplicates for the third element
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+
+                    # Move pointers inward
+                    left += 1
+                    right -= 1
+
+        return res
